903a409c309a7dcc10f85fec2730d52236837ff4
muskan-chouhan <mushkanc430@gmail.com>
2026-01-11 17:51:10 +0530

    again  19 add logic adult and minor

--- 19-studentform-list-project/src/component/StudentCard.jsx
+++ 19-studentform-list-project/src/component/StudentCard.jsx
@@ -1,28 +1,31 @@
 function StudentCard(props) {
     return (
         <>
-           
-
-                <div className="student-card">
-                    <h3 className="student-name">{props.name}</h3>
-
-                    <p className="student-info">
-                        <span className="label">Course:</span>
-                        <span className="value">{props.course}</span>
-                    </p>
-
-                    <p className="student-info">
-                        <span className="label">Age:</span>
-                        <span className="value adult">{props.age}</span>
-                    </p>
-
-                    <button
-                     className="select-btn">Select</button>
-                </div>
 
 
+            <div className="student-card">
+                <h3 className="student-name">{props.name}</h3>
 
-            
+                <p className="student-info">
+                    <span className="label">Course:</span>
+                    <span className="value">{props.course}</span>
+                </p>
+
+                <p className="student-info">
+                    {props.age >= 18 ? (
+                        <span className="value adult">Adult</span>
+                    ) : (
+                        <span className="value minor">Minor</span>
+                    )}
+                </p>
+
+                <button
+                    className="select-btn">Select</button>
+            </div>
+
+
+
+
 
         </>
     )
