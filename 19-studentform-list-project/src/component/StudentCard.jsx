0e64c3ae6f45fdbd4eb954240d48de4189dab9fe
muskan-chouhan <mushkanc430@gmail.com>
2026-01-12 22:33:54 +0530

    again  19 add - select remove logic

--- 19-studentform-list-project/src/component/StudentCard.jsx
+++ 19-studentform-list-project/src/component/StudentCard.jsx
@@ -1,36 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
+
 function StudentCard(props) {
+  const [selected, setSelected] = useState(false);
 
-    return (
-    
-        <>
-        
-            <div className="student-card">
-                <h3 className="student-name">{props.name}</h3>
+  function control() {
+    setSelected(!selected);
+  }
 
-                <p className="student-info">
-                    <span className="label">Course:</span>
-                    <span className="value">{props.course}</span>
-                </p>
+  return (
+    <div className="student-card">
+      <h3 className="student-name">{props.name}</h3>
 
-                <p className="student-info">
-                    {props.age >= 18 ? (
-                        <span className="value adult">Adult :{props.age}</span>
-                    ) : (
-                        <span className="value minor">Minor :{props.age}</span>
-                    )}
-                </p>
+      <p className="student-info">
+        <span className="label">Course:</span>
+        <span className="value">{props.course}</span>
+      </p>
 
-                <button
-                    className="select-btn">Select</button>
-            </div>
+      <p className="student-info">
+        {props.age >= 18 ? (
+          <span className="value adult">Adult : {props.age}</span>
+        ) : (
+          <span className="value minor">Minor : {props.age}</span>
+        )}
+      </p>
 
-
-
-
-
-        </>
-    )
+      <button className="select-btn" onClick={control}>
+        {selected ? "Remove" : "Select"}
+      </button>
+    </div>
+  );
 }
 
 export default StudentCard;
