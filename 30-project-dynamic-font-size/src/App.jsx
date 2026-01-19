65d5561eb0227be2f99dc31ecf2f23d7cca4a000
muskan-chouhan <mushkanc430@gmail.com>
2026-01-19 15:26:44 +0530

    add 30 project -dynamic font size of text

--- 30-project-dynamic-font-size/src/App.jsx
+++ 30-project-dynamic-font-size/src/App.jsx
@@ -18,3 +18,34 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// import { useState } from 'react'
+
+// function App() {
+//   const [text, setText] = useState('Small')
+
+//   return (
+//     <>
+//       <select onChange={(event) => setText(event.target.value)}>
+//         <option value="Small">Small</option>
+//         <option value="Medium">Medium</option>
+//         <option value="Large">Large</option>
+//       </select>
+
+//       {
+//         text === 'Small' && <h1 className="small">muskan</h1>
+//       }
+//       {
+//         text === 'Medium' && <h1 className="medium">muskan</h1>
+//       }
+//       {
+//         text === 'Large' && <h1 className="large">muskan</h1>
+//       }
+//     </>
+//   )
+// }
+
+// export default App
