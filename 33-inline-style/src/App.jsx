1005cb86aebc39b305c663af6e7b7f86ac68a52e
muskan-chouhan <mushkanc430@gmail.com>
2026-01-21 17:18:27 +0530

    add 33 inline style

--- 33-inline-style/src/App.jsx
+++ 33-inline-style/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <>
       <h1 style={{ color: 'red' }}>Inline Style in react JS</h1>
       <div style={{
-        display:"flex",
-        flexWrap : "wrap"
+        display: "flex",
+        flexWrap: "wrap"
       }}>
 
         <div style={cardStyle}>
@@ -145,6 +145,24 @@ function App() {
             <p>Software Devloper</p>
           </div>
         </div>
+
+                <div style={cardStyle}>
+          <img style={{
+            width: '200px'
+          }}
+
+            src='https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybHxlbnwwfHwwfHx8MA%3D%3D'></img>
+
+          <div style={{
+            padding: "10px"
+          }}>
+            <h2>rosy reter</h2>
+            <p>Software Devloper</p>
+          </div>
+        </div>
+
+
+        
 
       </div>
     </>
