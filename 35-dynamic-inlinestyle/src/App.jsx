67eca9c3ed4e3e3f7b0105ddb098766f1eedf0f9
muskan-chouhan <mushkanc430@gmail.com>
2026-01-23 13:53:13 +0530

    add 35 dynamic and conditional inline style

---
+++ 35-dynamic-inlinestyle/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #cccccc3b",
+    boxShadow: "1px 2px 3px 0px #cccccc57",
+    margin: '10px'
+  }
+
+  return (
+    <>
+      <h1 style={{ color: 'red' }}>Inline Style in react JS</h1>
+      <div style={{
+        display: "flex",
+        flexWrap: "wrap"
+      }}>
+
+        <div style={cardStyle}>
+          <img style={{
+            width: '200px'
+          }}
+
+            src='https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybHxlbnwwfHwwfHx8MA%3D%3D'></img>
+
+          <div style={{
+            padding: "10px"
+          }}>
+            <h2>rosy reter</h2>
+            <p>Software Devloper</p>
+          </div>
+        </div>
+
+
+
+        
+
+      </div>
+    </>
+  )
+}
+
+export default App
