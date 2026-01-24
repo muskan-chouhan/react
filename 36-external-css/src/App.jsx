5cb05ae66fe86dbec649477e745fca0f6b635eec
muskan-chouhan <mushkanc430@gmail.com>
2026-01-24 21:23:05 +0530

    add 36 - external css file

--- 36-external-css/src/App.jsx
+++ 36-external-css/src/App.jsx
@@ -30,6 +30,28 @@ function App() {
       
      </div>
 
+              <div className='card'>
+      <div>
+           <img className='img'  src='https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybHxlbnwwfHwwfHx8MA%3D%3D'></img>
+      </div>
+      <div className='text-wrap'>
+        <h2>peter alex</h2>
+        <p>Software Devloper</p>
+      </div>
+      
+     </div>
+
+              <div className='card'>
+      <div>
+           <img className='img'  src='https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybHxlbnwwfHwwfHx8MA%3D%3D'></img>
+      </div>
+      <div className='text-wrap'>
+        <h2>peter alex</h2>
+        <p>Software Devloper</p>
+      </div>
+      
+     </div>
+
 
 </div>
     </>
