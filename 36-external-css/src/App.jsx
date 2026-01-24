acb9fe6fcd12db8219b206861b9af2c63ad282e3
muskan-chouhan <mushkanc430@gmail.com>
2026-01-24 21:20:59 +0530

    add 36 - external css file

---
+++ 36-external-css/src/App.jsx
@@ -0,0 +1,29 @@
+import './css/style.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='heading'>External style</h1>
+<div className='container'>
+     <div className='card'>
+      <div>
+           <img className='img'  src='https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybHxlbnwwfHwwfHx8MA%3D%3D'></img>
+      </div>
+      <div className='text-wrap'>
+        <h2>peter alex</h2>
+        <p>Software Devloper</p>
+      </div>
+      
+     </div>
+
+
+
+</div>
+    </>
+  )
+}
+
+export default App
