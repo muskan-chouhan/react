ebeef21cb056d7c9ae91dbf4f304cb3113952a2c
muskan-chouhan <mushkanc430@gmail.com>
2026-01-26 14:04:07 +0530

    add 40 bootstrap

--- 40-add-bootstrap/src/App.jsx
+++ 40-add-bootstrap/src/App.jsx
@@ -1,13 +1,27 @@
 
 import {Button} from "react-bootstrap"
+import { Alert } from "react-bootstrap"
 function App() {
 
 
   return (
     <>
  <h2>Add Bootstrap in react js</h2>
- <button>Add</button><br></br>
- <Button>Add</Button>
+ <button>Add</button><br></br><br></br>
+ <Button >Add</Button><br></br><br></br>
+
+ <Alert variant="danger">hlw bootstrap installed</Alert>
+<br></br><br></br>
+<br></br><br></br>
+
+
+ <Button  variant="success" onClick={()=>alert('bootstrap button')}>bootstrap Button</Button>
+ <br></br><br></br>
+ <br></br><br></br>
+ <button  variant="success" onClick={()=>alert('bootstrap button')}>simple bootstrap</button>
+    
+    <br></br><br></br>
+    <br></br><br></br>
     </>
   )
 }
