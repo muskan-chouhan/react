6e28655dde0ab60a8411b4b76978c8d051584213
muskan-chouhan <mushkanc430@gmail.com>
2026-01-28 13:39:26 +0530

    add useFormSatatus

--- 45-useformstate/src/App.jsx
+++ 45-useformstate/src/App.jsx
@@ -2,14 +2,26 @@
 
 
 function App() {
+
+  const handelSubmit = async() =>{
+  await new Promise(res =>setTimeout(res,2000));
+    
+console.log('submit');
+
+  
+  }
   return (
     <>
+    <form action={handelSubmit}>
       <h1>UserFromSatatus Hook in recat js 19</h1>
       <input type='text' placeholder='Enter Name'></input>
       <br></br>
       <br></br>
       <input type='password' placeholder='Enter Password'></input>
+      <br>
+      </br>
       <button>Submit</button>
+      </form>
     </>
   )
 }
