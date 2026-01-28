14b6901df3efc0fb641f3a6e357ef92af5b29dde
muskan-chouhan <mushkanc430@gmail.com>
2026-01-28 13:49:08 +0530

    add useFormSatatus Hook in react js 19

--- 45-useformstate/src/App.jsx
+++ 45-useformstate/src/App.jsx
@@ -1,26 +1,38 @@
 
-
+import {useFormStatus} from 'react-dom'
 
 function App() {
-
+  
   const handelSubmit = async() =>{
   await new Promise(res =>setTimeout(res,2000));
     
 console.log('submit');
 
-  
   }
-  return (
-    <>
-    <form action={handelSubmit}>
-      <h1>UserFromSatatus Hook in recat js 19</h1>
-      <input type='text' placeholder='Enter Name'></input>
+
+  function CustomerForm (){
+     const  {pending} = useFormStatus();
+    return(
+      <>
+           <input type='text' placeholder='Enter Name'></input>
       <br></br>
       <br></br>
       <input type='password' placeholder='Enter Password'></input>
       <br>
       </br>
-      <button>Submit</button>
+      <button disabled={pending}>{pending?'submitting...' :'Submit'}</button>
+      <br>
+      </br>
+
+      </>
+    )
+  }
+  return (
+    <>
+      <h1>UserFromSatatus Hook in recat js 19</h1>
+    <form action={handelSubmit}>
+    <CustomerForm/>
+   
       </form>
     </>
   )
