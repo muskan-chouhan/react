31cea0549d6ed00cefd92c64baf4314244349370
muskan-chouhan <mushkanc430@gmail.com>
2026-01-28 12:50:36 +0530

    add 44 forwardRef

---
+++ 44-forwardref/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import UserInput from './component/userinput'
+import { useRef } from 'react'
+function App() {
+  const inputRef = useRef(null)
+
+  function updateInput (){
+inputRef.current.value = 200;
+inputRef.current.focus();
+
+  }
+
+  return (
+    <>
+    <h1>Forward Ref</h1>
+    <UserInput ref = {inputRef}/>
+ 
+    <button onClick={updateInput}>Update Input Field</button>
+    
+    </>
+  )
+}
+
+export default App
