64f5bc07a9606d5e1c8d2650cee4273bf570e6c5
muskan-chouhan <mushkanc430@gmail.com>
2026-01-29 18:58:46 +0530

    add 46  useTransition Hook with loader

--- 46-usetransition-hook/src/App.jsx
+++ 46-usetransition-hook/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useState, useTransition } from 'react'
 
 function App() {
-  const [pending, setPending] = useState(false)
+  const [pending, setTransition] = useTransition()
 const handel = async() => {
-  setPending(true)
+   setTransition( async ()=>{
     await new Promise(res => setTimeout(res,2000)) 
-       setPending(false)
+   })
+   
+      
   } 
   return (
     <>
  <h1>useTransition Hook in React js 19</h1>
-
+{
+  pending ?<img src='https://media.tenor.com/wfEN4Vd_GYsAAAAM/loading.gif' style={{width:"100px"}}></img>
+  :null
+}
 
  <button disabled ={pending} onClick={handel}>Click</button>
     </>
