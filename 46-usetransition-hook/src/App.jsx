db5bc90859e6c4c5603839319bdc96097fa67847
muskan-chouhan <mushkanc430@gmail.com>
2026-01-29 18:40:17 +0530

    add 46  useTransition Hook

--- 46-usetransition-hook/src/App.jsx
+++ 46-usetransition-hook/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [pending, setPending] = useState(false)
+  function handel(){
 
+    setPending(true)
+  } 
   return (
     <>
  <h1>useTransition Hook in React js 19</h1>
+
+
+ <button disabled ={pending} onClick={handel}>Click</button>
     </>
   )
 }
