dea9a04b54fe91f5bf6e4f8f2ffe8ce64cf862dd
muskan-chouhan <mushkanc430@gmail.com>
2026-01-29 18:13:54 +0530

    add useFormSatatus Hook in react js 19

--- 45-useformstate/src/App.jsx
+++ 45-useformstate/src/App.jsx
@@ -1,28 +1,28 @@
 
-import {useFormStatus} from 'react-dom'
+import { useFormStatus } from 'react-dom'
 
 function App() {
-  
-  const handelSubmit = async() =>{
-  await new Promise(res =>setTimeout(res,2000));
-    
-console.log('submit');
+
+  const handelSubmit = async () => {
+    await new Promise(res => setTimeout(res, 2000));
+
+    console.log('submit');
 
   }
 
-  function CustomerForm (){
-     const  {pending} = useFormStatus();
-    return(
+  function CustomerForm() {
+    const { pending } = useFormStatus();
+    return (
       <>
-           <input type='text' placeholder='Enter Name'></input>
-      <br></br>
-      <br></br>
-      <input type='password' placeholder='Enter Password'></input>
-      <br>
-      </br>
-      <button disabled={pending}>{pending?'submitting...' :'Submit'}</button>
-      <br>
-      </br>
+        <input type='text' placeholder='Enter Name'></input>
+        <br></br>
+        <br></br>
+        <input type='password' placeholder='Enter Password'></input>
+        <br>
+        </br>
+        <button disabled={pending}>{pending ? 'submitting...' : 'Submit'}</button>
+        <br>
+        </br>
 
       </>
     )
@@ -30,9 +30,9 @@ console.log('submit');
   return (
     <>
       <h1>UserFromSatatus Hook in recat js 19</h1>
-    <form action={handelSubmit}>
-    <CustomerForm/>
-   
+      <form action={handelSubmit}>
+        <CustomerForm />
+
       </form>
     </>
   )
