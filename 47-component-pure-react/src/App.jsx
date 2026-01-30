5969778463f630fa4edbe6af47c8da341965c252
muskan-chouhan <mushkanc430@gmail.com>
2026-01-30 14:08:19 +0530

    add 47 pure component

---
+++ 47-component-pure-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Pure componenet and impure component</h1>
+    </>
+  )
+}
+
+export default App
