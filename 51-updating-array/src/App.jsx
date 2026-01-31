e9d5b858dc0be13ea2b05f5c025a5de1525ac6c2
muskan-chouhan <mushkanc430@gmail.com>
2026-01-31 13:45:03 +0530

    51- using array updating last element

--- 51-updating-array/src/App.jsx
+++ 51-updating-array/src/App.jsx
@@ -3,16 +3,31 @@ import { useState } from 'react'
 
 function App() {
   const [data, setData] = useState(['muskan','neha','mahak'])
+ function handelUser(newName){
+  // setData([name]); //isse pehle ka data gayab ho jayga excitsing data remove,humko pehle ka data bhi rkhnahai ,
+   data[data.length-1] = newName
+   console.log(data);
+   setData([...data]) //ek new aaray banspread kiya then its work
+   
 
+  
+ }
   return (
     <>
     <h1 style={{textAlign:'center'}}>Updating Array in state</h1>
+
+  <input 
+  type="text" 
+  placeholder="enter last user name" 
+  onChange={(event)=>handelUser(event.target.value)}
+/>
+
     {
       data.map((item,index)=>(
         <h3 key={index}>{item}</h3>
       ))
     }
-    <button onClick={()=>setName('muskan chouhan')}>Updating Name</button>
+    
  
     </>
   )
