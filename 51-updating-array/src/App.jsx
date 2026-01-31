67c49995dc3ccb80f5c9b71062c70117ef8013c9
muskan-chouhan <mushkanc430@gmail.com>
2026-01-31 13:23:32 +0530

    51- using array simply show names

--- 51-updating-array/src/App.jsx
+++ 51-updating-array/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 
 function App() {
-  const [name, setName] = useState('muskan')
+  const [data, setData] = useState(['muskan','neha','mahak'])
 
   return (
     <>
     <h1 style={{textAlign:'center'}}>Updating Array in state</h1>
-    <h2>{name}</h2>
+    {
+      data.map((item,index)=>(
+        <h3 key={index}>{item}</h3>
+      ))
+    }
     <button onClick={()=>setName('muskan chouhan')}>Updating Name</button>
  
     </>
