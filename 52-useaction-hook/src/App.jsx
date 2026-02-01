f5da265c9bb00e5b6584c72eb031a9a054aa75e5
muskan-chouhan <mushkanc430@gmail.com>
2026-02-01 18:19:41 +0530

    52 - create state

--- 52-useaction-hook/src/App.jsx
+++ 52-useaction-hook/src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
+import { useActionState } from 'react'
 
 
 function App() {
-  const [data,action,pending] = useState()
+  const [data, action, pending] = useActionState()
 
   return (
     <>
-    <h1 style={{textAlign:"center"}}>UseAction Hook</h1>
-    <form action={action}>
-    <input 
-      type='text'name='name'placeholder='enter name'
-    />
-    <br/><br/>
-    <input 
-      type='password'name='password'placeholder='enter password'
-    />
-    <br/><br/>
-    <button>submit</button>
-</form>
+      <h1 style={{ textAlign: "center" }}>UseAction Hook</h1>
+      <form action={action}>
+        <input
+          type='text' name='name' placeholder='enter name'
+        />
+        <br /><br />
+        <input
+          type='password' name='password' placeholder='enter password'
+        />
+        <br /><br />
+        <button>submit</button>
+      </form>
     </>
   )
 }
