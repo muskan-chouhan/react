82c344611bebcab73a9c2a46b01a1c8d44bc3d43
muskan-chouhan <mushkanc430@gmail.com>
2026-02-01 20:54:01 +0530

    52 - create state

--- 52-useaction-hook/src/App.jsx
+++ 52-useaction-hook/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import { useActionState } from 'react'
 
+function handelSubmit(){
+  console.log('handelSubmit called');
+  
+}
 
 function App() {
-  const [data, action, pending] = useActionState()
+  const [data, action, pending] = useActionState(handelSubmit,undefined)
 
   return (
     <>
