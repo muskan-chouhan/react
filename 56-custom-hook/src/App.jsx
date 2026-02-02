857341f675e271b7d9c839b8c75c6bf9ccd8ba7c
muskan-chouhan <mushkanc430@gmail.com>
2026-02-02 18:30:57 +0530

    55- import custom hook

--- 56-custom-hook/src/App.jsx
+++ 56-custom-hook/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-
-
+import useToggle from './useToggle'
 function App() {
+  const[value,toggleValue] =useToggle(true)
+  console.log('val-----',value);
   
-
   return (
     <>
-     <h1>Custom Hooks</h1>
+     
+     <button>Toggle Heading</button>
+     <button>Show Heading</button>
+     <button>Hide Heading</button> 
+     {
+      value?<h1>Custom Hooks IN React Js</h1>
+      :null
+     }
     </>
   )
 }
