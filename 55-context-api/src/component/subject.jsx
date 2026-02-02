1e1cc612be3841c1226fe7310ce1fa5284fcc54a
muskan-chouhan <mushkanc430@gmail.com>
2026-02-02 17:39:28 +0530

    54- show data using usecontext

--- 55-context-api/src/component/subject.jsx
+++ 55-context-api/src/component/subject.jsx
@@ -1,9 +1,10 @@
+import { useContext } from "react";
+import { SubjectContext } from "./contextData";
 const Subject = () => {
-
+const subject = useContext(SubjectContext)
   return (
   <div style={{backgroundColor:"#f2e6ff", padding:"20px"}} >
-    <h1>Subject component</h1>
-
+    <h1>Subject is : {subject}</h1>
   </div>
   )
 }
