3ac5110824ded8fb4b44aa40c2b69b107ad6f53a
muskan-chouhan <mushkanc430@gmail.com>
2026-02-02 14:47:12 +0530

    54- see the problem to using div as a parent element

--- 54-fragment/src/App.jsx
+++ 54-fragment/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { Fragment } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
- <div>
-     <h1 style={{textAlign:'center'}}>Fragment in react js</h1>
-     <h3>fragment</h3>  
-     <h3>fragment</h3>
-   </div>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Fragment in react js</h1>
+      <Data />
+      <Data />
+      <Data />
+      <Data />
+      <Data />
+
+    </div>
   )
 }
 
 export default App
+
+const Data = () => {
+  return (
+    <div>
+      <h3>fragment</h3>
+      <h3>fragment</h3>
+    </div>
+  )
+}
 
