541c6b0c78fcb95798de239e46db73b5ef0ea129
muskan-chouhan <mushkanc430@gmail.com>
2026-02-02 14:36:18 +0530

    54- paas element and objects only through one parent div , but this create issue also

---
+++ 54-fragment/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <div>
+     <h1 style={{textAlign:'center'}}>Fragment in react js</h1>
+     <h3>fragment</h3>  
+     <h3>fragment</h3>
+   </div>
+  )
+}
+
+export default App
+
