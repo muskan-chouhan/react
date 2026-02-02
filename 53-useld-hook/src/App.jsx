81d962671ce400bb09c4e25b48d9e44e6075581a
muskan-chouhan <mushkanc430@gmail.com>
2026-02-02 14:11:18 +0530

    53 -get new input ids using useid hook

--- 53-useld-hook/src/App.jsx
+++ 53-useld-hook/src/App.jsx
@@ -2,21 +2,34 @@ import { useState } from 'react'
 import { useId } from 'react'
 
 function App() {
-  const name = useId();
-  const password = useId();
-  const age = useId();
-  const skills = useId();
 
   return (
     <>
-    <h1 style={{textAlign:'center'}}>Use id hook</h1>
-    <h2>{name}</h2>
+      <h1 style={{ textAlign: 'center' }}>Use id hook</h1>
+      {/* <h2>{name}</h2>
     <h2>{password}</h2>
     <h2>{age}</h2>
-    <h2>{skills}</h2>
+    <h2>{skills}</h2> */}
+  <UserForm/>
+  <UserForm/>
 
     </>
   )
 }
 
 export default App
+
+const UserForm = () => {
+  const name = useId();
+  const password = useId();
+  const age = useId();
+  const skills = useId();
+  return (
+    <>
+      <form action=''>
+        <label htmlFor={name}>Enter user name</label>
+        <input type='text' id={name} placeholder='enter name' />
+      </form>
+    </>
+  )
+}
