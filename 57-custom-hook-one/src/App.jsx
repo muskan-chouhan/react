48d0e6e0f8d23228fc090a1a72f5b3ec5fe27eb3
muskan-chouhan <mushkanc430@gmail.com>
2026-02-03 17:36:40 +0530

    57-using usestate

--- 57-custom-hook-one/src/App.jsx
+++ 57-custom-hook-one/src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
-
 function App() {
-
+  const [count, setCount] = useState(0)
+  function increment(){
+    setCount(count+1)
+  }
+   function decrement(){
+    setCount(count-1)
+  }
   return (
     <>
      <h1>Custom hook practice</h1>
+     <div>{count}</div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </>
   )
 }
