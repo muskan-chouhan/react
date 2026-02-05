c31f8f51f9d6d7947b3d082fe20b2c961d2bff97
muskan-chouhan <mushkanc430@gmail.com>
2026-02-05 15:52:37 +0530

    import link

--- router-basic/src/App.jsx
+++ router-basic/src/App.jsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route,Link} from 'react-router';
 import Home from './Home';
 import About from './about';
 function App() {
   return (
+    <>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
     </Routes>
+    </>
   );
 }
 
