57eccd60da90f14c353ebb4e82cb05eb30fd62ac
muskan-chouhan <mushkanc430@gmail.com>
2026-02-05 17:37:22 +0530

    add links

--- router-in-react19/src/App.jsx
+++ router-in-react19/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
  
   return (
     <>
+          <Link to='/'>Home</Link>
+          <br/>
+          <Link to='/about'>About</Link>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
