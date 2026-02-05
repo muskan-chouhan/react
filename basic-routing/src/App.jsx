f96abed2ae9ef9543e488440590e7e9abd46248f
muskan-chouhan <mushkanc430@gmail.com>
2026-02-05 18:09:15 +0530

    about Routes ,Routes and Link

--- basic-routing/src/App.jsx
+++ basic-routing/src/App.jsx
@@ -1,11 +1,20 @@
-import {Route, Routes} from 'react-router'
+import {Route, Routes,Link} from 'react-router'
+import Home from './Home'
+import About from './About'
+import Login from './Login'
 function App() {
 
   return (
     <>
+    <Link to='/' ><h1>Home</h1></Link>
+    <Link to='/about' ><h1>About</h1></Link>
+    <Link to='/login' ><h1>Login</h1></Link>
     <Routes>
-      <Route path='/' element={<h1>Basic Page Building with react-routing</h1>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
+
    
     </>
   )
