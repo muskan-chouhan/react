c3ccc57885029629053bcbadc5666d9b72cedd22
muskan-chouhan <mushkanc430@gmail.com>
2026-02-05 18:15:13 +0530

    create seprate file for links and import in app file

--- basic-routing/src/App.jsx
+++ basic-routing/src/App.jsx
@@ -1,14 +1,13 @@
-import {Route, Routes,Link} from 'react-router'
+import {Route, Routes} from 'react-router'
 import Home from './Home'
 import About from './About'
 import Login from './Login'
+import NavBar from './NavBar'
 function App() {
 
   return (
     <>
-    <Link to='/' ><h1>Home</h1></Link>
-    <Link to='/about' ><h1>About</h1></Link>
-    <Link to='/login' ><h1>Login</h1></Link>
+    <NavBar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
---
+++ basic-routing/src/NavBar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router"
+export default function NavBar() {
+    return (
+        <div>
+            <Link to='/' ><h1>Home</h1></Link>
+            <Link to='/about' ><h1>About</h1></Link>
+            <Link to='/login' ><h1>Login</h1></Link>
+        </div>
+    )
+}
