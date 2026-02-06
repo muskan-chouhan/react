2c1dc1186a64ebd612e3fe375800f476267ca190
muskan-chouhan <mushkanc430@gmail.com>
2026-02-06 18:28:09 +0530

    layout-routes

---
+++ layout-n-index-routes/src/App.jsx
@@ -0,0 +1,37 @@
+import {Navigate, Route, Routes} from 'react-router'
+import Home from './Home'
+import About from './About'
+import College from './College'
+import Student from './Student'
+import Department from './Department'
+import Header from './header'
+import './Navbar.css'
+function App() {
+
+  return (
+    <>
+    {/* <Header/> */}
+    <Routes>
+
+      <Route element={<Header/>}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Route>
+
+      
+      
+      
+       <Route path='/College' element={<College/>}>
+         <Route path='student' element={<Student/>}/>
+         <Route path='department' element={<Department/>}/>
+      </Route>
+    </Routes>
+    
+
+
+   
+    </>
+  )
+}
+
+export default App
---
+++ layout-n-index-routes/src/College.jsx
@@ -0,0 +1,15 @@
+import {Link,NavLink,Outlet} from 'react-router'
+
+function College() {
+  return (
+    <div className='college' style={{textAlign:'center'}}>
+    <h1>College</h1>
+    <Link to='/'>Go Back to Home</Link>
+    <NavLink className='link' to='student'>Student</NavLink>
+    <NavLink className='link' to='department'>Department</NavLink>
+    <Outlet/>
+    </div>
+  )
+}
+
+export default College
