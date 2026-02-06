360a88f87592b5ae581408131f8f8e4ad59702ae
muskan-chouhan <mushkanc430@gmail.com>
2026-02-06 15:09:57 +0530

    create 4O4 page

--- redirection-and-404/src/PageNotFound.jsx
+++ redirection-and-404/src/PageNotFound.jsx
@@ -1,9 +1,11 @@
 function PageNotFound() {
   return (
-    <>
+ 
+    <div style={{textAlign:'center'}}>
     <h1>PageNotFound</h1>
     <h2>4O4 Error</h2>
-    </>
+    <img style={{width:'60%'}} src="https://www.imarkinfotech.com/wp-content/uploads/2017/10/404-Page-Cover.png"></img>
+    </div>
   )
 }
 
