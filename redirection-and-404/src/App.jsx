1ac0887543188240385381783def506dff8b220d
muskan-chouhan <mushkanc430@gmail.com>
2026-02-06 14:59:05 +0530

    create 4O4 page

--- redirection-and-404/src/App.jsx
+++ redirection-and-404/src/App.jsx
@@ -4,6 +4,7 @@ import About from './About'
 import Login from './Login'
 import NavBar from './NavBar'
 import './Navbar.css'
+import PageNotFound  from './PageNotFound'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
     </Routes>
 
    
--- redirection-and-404/src/PageNotFound.jsx
+++ redirection-and-404/src/PageNotFound.jsx
@@ -2,6 +2,7 @@ function PageNotFound() {
   return (
     <>
     <h1>PageNotFound</h1>
+    <h2>4O4 Error</h2>
     </>
   )
 }
