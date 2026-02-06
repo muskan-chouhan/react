010b314c496ef85140cb8abe80dce34864490879
muskan-chouhan <mushkanc430@gmail.com>
2026-02-06 15:23:00 +0530

    redirect/navigation

--- redirection-and-404/src/App.jsx
+++ redirection-and-404/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router'
+import {Navigate, Route, Routes} from 'react-router'
 import Home from './Home'
 import About from './About'
 import Login from './Login'
@@ -14,7 +14,8 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='*' element={<PageNotFound/>}/>
+      {/* <Route path='*' element={<PageNotFound/>}/> */}
+       <Route path='*' element={<Navigate to='/login'/>}/>
     </Routes>
 
    
--- redirection-and-404/src/PageNotFound.jsx
+++ redirection-and-404/src/PageNotFound.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router"
+
 function PageNotFound() {
   return (
  
     <div style={{textAlign:'center'}}>
     <h1>PageNotFound</h1>
-    <h2>4O4 Error</h2>
-    <img style={{width:'60%'}} src="https://www.imarkinfotech.com/wp-content/uploads/2017/10/404-Page-Cover.png"></img>
+    <div><Link to='/'>Go to Home Page</Link></div>
+    <img style={{width:'50%', marginTop:'5px'}} src="https://www.imarkinfotech.com/wp-content/uploads/2017/10/404-Page-Cover.png"></img>
     </div>
   )
 }
