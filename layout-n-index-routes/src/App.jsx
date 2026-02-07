1913eaf26439d965ee442c000c1ee0cfcdd64fd5
muskan-chouhan <mushkanc430@gmail.com>
2026-02-07 22:00:04 +0530

    show default child page content using index

--- layout-n-index-routes/src/App.jsx
+++ layout-n-index-routes/src/App.jsx
@@ -1,18 +1,33 @@
-import {Route, Routes} from 'react-router'
+import {Navigate, Route, Routes} from 'react-router'
 import Home from './Home'
 import About from './About'
-import Login from './Login'
-import NavBar from './NavBar'
+import College from './College'
+import Student from './Student'
+import Department from './Department'
+import Header from './header'
+import './Navbar.css'
 function App() {
 
   return (
     <>
-    <NavBar/>
+    {/* <Header/> */}
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/login' element={<Login/>}/>
+
+      <Route element={<Header/>}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Route>
+
+      
+      
+      
+       <Route path='/College' element={<College/>}>
+         <Route index element={<Student/>}/>
+         <Route path='department' element={<Department/>}/>
+      </Route>
     </Routes>
+    
+
 
    
     </>
