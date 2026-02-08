8dc638a9c0aa96023e8a28d1e4c7141abf141d9d
muskan-chouhan <mushkanc430@gmail.com>
2026-02-08 17:17:24 +0530

    add dynamic route file

---
+++ dynamic-routing/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+    <h1>Dynamic route</h1>
+    </>
+  )
+}
+
+export default App
---
+++ dynamic-routing/src/NavBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router"
+import './header.css'
+export default function NavBar() {
+    return (<div className="header">
+        <div>
+            <Link className="link"  to='/'><h2>logo</h2></Link>
+        </div>
+        <div>
+            <ul>
+                <li><Link className="link" to= '/'>Home</Link></li>
+                <li><Link className="link" to= '/about'>About</Link></li>
+                <li><Link className="link" to= '/login'>login</Link></li>
+            </ul>
+        </div>
+    </div>
+
+    )
+}
