bb5f11cba29deb66b8900d14cc11198e68b0b9c1
muskan-chouhan <mushkanc430@gmail.com>
2026-02-08 18:04:07 +0530

    show user id on user details page

--- dynamic-routing/src/UserDetails.jsx
+++ dynamic-routing/src/UserDetails.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router"
 function UserDetails() {
+    const paramsData = useParams();
+    console.log(paramsData.id);
+    
   return (
     <div style={{marginLeft:'20px'}}>
     <h1>User Detail</h1>
+    <h2>User id is :{paramsData.id }</h2>
 
    </div>
   )
