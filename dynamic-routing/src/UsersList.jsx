2d892f66c2c3c6953c079fe8b0e49e3dfb905c15
muskan-chouhan <mushkanc430@gmail.com>
2026-02-08 17:41:39 +0530

    show user list

--- dynamic-routing/src/UsersList.jsx
+++ dynamic-routing/src/UsersList.jsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router"
 function UsersList() {
-      const users = [
+      const userData = [
       { id:1 , name:'anil' },
       { id:2 , name:'mohan' },
       { id:3 , name:'shyam' },
       { id:4 , name:'rahul' },
     ]
   return (
-    <>
+    <div style={{marginLeft:'20px'}}>
     <h1>User List Page</h1>
-    </>
+    {
+      userData.map((data)=>(
+        <div>
+          <h3><Link to = '' >{data.name}</Link></h3>
+        </div>
+      ))
+    }
+   </div>
   )
 }
 
