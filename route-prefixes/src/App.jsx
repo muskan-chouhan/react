d54a6c9970d10a847e8952a65c1e2a840b64d487
muskan-chouhan <mushkanc430@gmail.com>
2026-02-08 14:21:19 +0530

    add route prefixes file

--- route-prefixes/src/App.jsx
+++ route-prefixes/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
+import {Navigate, Route, Routes} from 'react-router'
+import Home from './home'
+import About from './About'
+import College from './College'
+import Student from './Student'
+import Department from './Department'
+import Header from './header'
+import './Navbar.css'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Route PreFixes</h1>
+    {/* <Header/> */}
+    <Routes>
+
+      <Route element={<Header/>}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Route>
+
+      
+      
+      
+       <Route path='/College' element={<College/>}>
+         <Route index element={<Student/>}/>
+         <Route path='department' element={<Department/>}/>
+      </Route>
+    </Routes>
+    
+
+
+   
     </>
   )
 }
---
+++ route-prefixes/src/Department.jsx
@@ -0,0 +1,9 @@
+function Department() {
+  return (
+    <>
+    <h1>Department Page</h1>
+    </>
+  )
+}
+
+export default Department
