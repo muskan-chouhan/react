748b62a07872e1881cfc3ad4bb8db93639e2cf3e
muskan-chouhan <mushkanc430@gmail.com>
2026-02-09 14:28:58 +0530

    add optional segment

--- dynamic-routing/src/App.jsx
+++ dynamic-routing/src/App.jsx
@@ -15,8 +15,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/users' element={<UsersList/>}/>
-      <Route path='/users/list' element={<UsersList/>}/>
+      <Route path='/users/list?' element={<UsersList/>}/>
       <Route path='/users/:id' element={<UserDetails/>}/>
       
       <Route path='*' element={<PageNotFound/>}/>
