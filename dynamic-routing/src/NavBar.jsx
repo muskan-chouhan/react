90aca5cbe9ec8c0ecd4bd1a5b27d59c4a5b01fd5
muskan-chouhan <mushkanc430@gmail.com>
2026-02-09 15:19:47 +0530

    change navbar file , link into navlink

--- dynamic-routing/src/NavBar.jsx
+++ dynamic-routing/src/NavBar.jsx
@@ -3,15 +3,15 @@ import './header.css'
 export default function NavBar() {
     return (<div className="header">
         <div>
-            <Link className="link"  to='/'><h2>logo</h2></Link>
+            <NavLink className="link"  to='/'><h2>logo</h2></NavLink>
         </div>
         <div>
             <ul>
                 <li><NavLink className="link" to= '/'>Home</NavLink></li>
-                <li><Link className="link" to= '/about'>About</Link></li>
-                <li><Link className="link" to= '/login'>login</Link></li>
-                <li><Link className="link" to= '/users'>Users</Link></li>
-                <li><Link className="link" to= '/users/list'>List</Link></li>
+                <li><NavLink className="link" to= '/about'>About</NavLink></li>
+                <li><NavLink className="link" to= '/login'>login</NavLink></li>
+                <li><NavLink className="link" to= '/users'>Users</NavLink></li>
+                <li><NavLink className="link" to= '/users/list'>List</NavLink></li>
             </ul>
         </div>
     </div>
