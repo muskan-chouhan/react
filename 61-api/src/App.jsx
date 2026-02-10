170866d15fbbfd56f53a128da1ea1ceb95a2fcf8
muskan-chouhan <mushkanc430@gmail.com>
2026-02-10 17:47:43 +0530

    api call succesful

--- 61-api/src/App.jsx
+++ 61-api/src/App.jsx
@@ -2,14 +2,14 @@ import { useEffect } from "react"
 
 function App() {
   useEffect(()=>{
-
-  })
+         getUserData()
+  },[])
 
   async function getUserData() {
     const url = 'https://dummyjson.com/users'
     let response = await fetch(url) 
     response = await response.json() 
-   
+    console.log(response);
     
   } 
   return (
