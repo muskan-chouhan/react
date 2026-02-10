6232e860235026e9ca890ef3688174e4f76e13b5
muskan-chouhan <mushkanc430@gmail.com>
2026-02-10 17:36:20 +0530

    set Fetch Data From Api

--- 61-api/src/App.jsx
+++ 61-api/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
-     function userData(){
-    const url = 'https://dummyjson.com/users' 
-   } 
+  function getUserData() {
+    const url= 'https://dummyjson.com/users'
+  }
   return (
     <>
       <h1>Fetch Data From Api</h1>
