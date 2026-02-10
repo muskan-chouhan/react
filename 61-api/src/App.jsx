b574b6f413bd6fd3f00c4031d5a514fd4f316ee4
muskan-chouhan <mushkanc430@gmail.com>
2026-02-10 18:19:43 +0530

    fetch api data and render on page and add some style

--- 61-api/src/App.jsx
+++ 61-api/src/App.jsx
@@ -1,37 +1,37 @@
 import { useEffect, useState } from "react"
 import './App.css'
 function App() {
-  const [usersData,setUsersData] = useState([])
-  useEffect(()=>{
-         getUserData()
-  },[])
+  const [usersData, setUsersData] = useState([])
+  useEffect(() => {
+    getUserData()
+  }, [])
 
   async function getUserData() {
     const url = 'https://dummyjson.com/users'
-    let response = await fetch(url) 
-    response = await response.json() 
+    let response = await fetch(url)
+    response = await response.json()
     // console.log(response.users);
     setUsersData(response.users)
-  } 
+  }
   // console.log(usersData);
-  
+
   return (
     <>
       <h1>Fetch Data From Api</h1>
-              <ul className="user-list">
-          <li>First Name</li>
-          <li>Last Name</li>
-          <li>Age</li>
-        </ul>
+      <ul className="user-list user-list-head">
+        <li>First Name</li>
+        <li>Last Name</li>
+        <li>Age</li>
+      </ul>
       {
-       usersData && usersData.map((user)=>(
-        <ul className="user-list">
-          <li>{user.firstName}</li>
-          <li>{user.lastName}</li>
-          <li>{user.age}</li>
-        </ul>
-       
-       ))
+        usersData && usersData.map((user) => (
+          <ul className="user-list">
+            <li>{user.firstName}</li>
+            <li>{user.lastName}</li>
+            <li>{user.age}</li>
+          </ul>
+
+        ))
       }
     </>
   )
