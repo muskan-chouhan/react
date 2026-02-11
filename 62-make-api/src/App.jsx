e3be47a0efd9052d4b2652bbac8a4f383bfb75f1
muskan-chouhan <mushkanc430@gmail.com>
2026-02-11 18:14:58 +0530

    show users data and some css

--- 62-make-api/src/App.jsx
+++ 62-make-api/src/App.jsx
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
+import './App.css'
 
- function App() {
-  const [usersData,setUsersData]=useState([])
-  useEffect(()=>{
+function App() {
+  const [usersData, setUsersData] = useState([])
+  useEffect(() => {
     getUsersData()
- },[])
+  }, [])
 
-  async function getUsersData(){
-   const url = 'http://localhost:3000/users'
-   let response = await fetch(url);
-   response = await response.json()
-   // console.log(response);
+  async function getUsersData() {
+    const url = 'http://localhost:3000/users'
+    let response = await fetch(url);
+    response = await response.json()
+    // console.log(response);
     setUsersData(response)
     console.log(usersData);
-    
-  
+
+
   }
   return (
     <>
-     <h1>install JSON server and Make API</h1>
-     {
-      usersData.map((data,index)=>(
-        <ul key={index}>
-          <li>{data.name}</li>
-        </ul>
-      ))
-     }
+      <h1>install JSON server and Make API</h1>
+      {
+        usersData && usersData.map((user) => (
+          <ul className="user-list">
+            <li>{user.name}</li>
+            <li>{user.age}</li>
+            <li>{user.email}</li>
+          </ul>
+
+        ))
+      }
     </>
   )
 }
