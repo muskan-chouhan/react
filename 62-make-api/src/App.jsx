715fcd3067bac9b0246623d5394bab703e626231
muskan-chouhan <mushkanc430@gmail.com>
2026-02-11 18:01:52 +0530

    intigratio start

--- 62-make-api/src/App.jsx
+++ 62-make-api/src/App.jsx
@@ -1,5 +1,12 @@
 
-function App() {
+async function App() {
+  function getUsersData(){
+   const url = 'http://localhost:3000/users'
+   const response = await fetch(url);
+   response = await response.json()
+  console.log(response);
+  
+  }
   return (
     <>
      <h1>install JSON server and Make API</h1>
