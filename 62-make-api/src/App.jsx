47ac16971ae54cf5b54ab720e3748950312ed9b5
muskan-chouhan <mushkanc430@gmail.com>
2026-02-11 18:11:32 +0530

    show users data

--- 62-make-api/src/App.jsx
+++ 62-make-api/src/App.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
  function App() {
-
+  const [usersData,setUsersData]=useState([])
   useEffect(()=>{
     getUsersData()
  },[])
@@ -10,12 +10,22 @@ import { useEffect } from "react";
    const url = 'http://localhost:3000/users'
    let response = await fetch(url);
    response = await response.json()
-  console.log(response);
+   // console.log(response);
+    setUsersData(response)
+    console.log(usersData);
+    
   
   }
   return (
     <>
      <h1>install JSON server and Make API</h1>
+     {
+      usersData.map((data,index)=>(
+        <ul key={index}>
+          <li>{data.name}</li>
+        </ul>
+      ))
+     }
     </>
   )
 }
