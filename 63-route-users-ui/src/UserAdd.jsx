a1b305604ee99ce2f9718d76a9573e0bae1498fd
muskan-chouhan <mushkanc430@gmail.com>
2026-02-12 18:09:53 +0530

    make states for input fields

--- 63-route-users-ui/src/UserAdd.jsx
+++ 63-route-users-ui/src/UserAdd.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
+
 export default function UserAdd(){
+    const [name,setName]=useState('')
+    const [age,setAge]=useState('')
+    const [email,setEmail]=useState('')
     return(
         <div style={{textAlign:"center"}}>
             <h1>Add New User</h1>
             <input
                 type="text" 
-                placeholder="Enter Name">
+                placeholder="Enter Name"
+                onChange={(e)=>setName(e.target.value)}>
             </input>
             <br></br><br></br>
             <input
