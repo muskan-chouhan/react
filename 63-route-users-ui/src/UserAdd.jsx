57ef7eaee0f7b069fc6a696adea64d6ce22d0d41
muskan-chouhan <mushkanc430@gmail.com>
2026-02-12 18:04:14 +0530

    make input field for add user page

--- 63-route-users-ui/src/UserAdd.jsx
+++ 63-route-users-ui/src/UserAdd.jsx
@@ -1,5 +1,21 @@
 export default function UserAdd(){
     return(
-        <h1>Add New User</h1>
+        <div>
+            <h1>Add New User</h1>
+            <input
+                type="text" 
+                placeholder="Enter Name">
+            </input>
+            <br></br><br></br>
+            <input
+                type="number" 
+                placeholder="Enter Age">
+            </input>
+            <br></br><br></br>
+            <input
+                type="email" 
+                placeholder="Enter Email">
+            </input>
+        </div>
     )
 }
