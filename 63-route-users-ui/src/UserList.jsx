b8669f7731f367ac6c5fea2beabcc9abb54c843b
muskan-chouhan <mushkanc430@gmail.com>
2026-02-13 16:07:41 +0530

    create delete function

--- 63-route-users-ui/src/UserList.jsx
+++ 63-route-users-ui/src/UserList.jsx
@@ -39,7 +39,7 @@ function UserList() {
                             <li>{user.name}</li>
                             <li>{user.age}</li>
                             <li>{user.email}</li>
-                            <li><button>Delete</button></li>
+                            <li><button onClick={()=>deleteUser(user.id)}>Delete</button></li>
                         </ul>
                     ))
                     : <h1>data loading...</h1>
