2f80bcba63c2a2c70cd01ac511cd274ff44d1aa4
muskan-chouhan <mushkanc430@gmail.com>
2026-02-14 14:35:00 +0530

    create inptu field in user edit pages

--- 63-route-users-ui/src/UserEdit.jsx
+++ 63-route-users-ui/src/UserEdit.jsx
@@ -2,6 +2,10 @@ function UserEdit(){
     return(
         <div style={{textAlign:"center"}}>
             <h1>User Edit Page</h1>
+            <input
+                type="text"
+                placeholder="user name">
+            </input>
         </div>
     )
 }
