cc8dd63f5e50d0673b7225ea8bfc0fe2dc4199ac
muskan-chouhan <mushkanc430@gmail.com>
2026-02-14 15:45:11 +0530

    using url with particular user

--- 63-route-users-ui/src/UserEdit.jsx
+++ 63-route-users-ui/src/UserEdit.jsx
@@ -1,23 +1,27 @@
 import { useEffect } from "react";
 import { useParams } from "react-router";
+import { useState } from "react"
+function UserEdit() {
+        const [name,setName]=useState('')
+        const [age,setAge]=useState('')
+        const [email,setEmail]=useState('')
+    const { id } = useParams();
+    //   console.log(id);
 
-function UserEdit(){
-  const {id} = useParams();
-//   console.log(id);
-  
-  useEffect(()=>{
-     getUserData();
-  },[])
+    useEffect(() => {
+        getUserData();
+    }, [])
 
-  const getUserData= async()=>{
-    // console.log(id);
-    const url = "http://localhost:3000/users/"+id
-    console.log(url);
-    
-    
-  }
-    return(
-        <div style={{textAlign:"center"}}>
+    const getUserData = async () => {
+        // console.log(id);
+        const url = "http://localhost:3000/users/" + id
+        // console.log(url);
+        let response = await fetch(url);
+        response = await response.json()
+        // console.log(response); 
+    }
+    return (
+        <div style={{ textAlign: "center" }}>
             <h1>User Edit Page</h1>
             <input
                 type="text"
