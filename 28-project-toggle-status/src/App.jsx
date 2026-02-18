77ab1d8fb4a8518f77252b0515c61bd29e2a3494
muskan-chouhan <mushkanc430@gmail.com>
2026-02-18 17:43:06 +0530

    create function for toggle status

--- 28-project-toggle-status/src/App.jsx
+++ 28-project-toggle-status/src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 
 function App() {
   const [status,setStatus] = useState(true)
+  const statusCheck = ()=>{
+    setStatus(!status)
+  }
   return (
     <>
-    <button>Toggle Status</button>
+    <button onClick={statusCheck}>Toggle Status</button>
     <br></br>
     <br></br>
+    <div>
+      <h1>{status?'Online':'Offline'}</h1>
+    </div>
     </>
   )
 }
