657958fa635979f0b788729c127ebf3d1a5cd890
muskan-chouhan <mushkanc430@gmail.com>
2026-02-18 18:04:09 +0530

    change badge color basis of condition

--- 28-project-toggle-status/src/App.jsx
+++ 28-project-toggle-status/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
-
+import "./App.css";
 function App() {
-  const [status,setStatus] = useState(true)
-  const statusCheck = ()=>{
+  const [status, setStatus] = useState(true)
+  const statusCheck = () => {
     setStatus(!status)
   }
   return (
     <>
-    <button onClick={statusCheck} >Toggle Status</button>
-    <br></br>
-    <br></br>
-    <div>
-      <h1 style={{ color: status ? "green" : "red" }}>{status?'Online':'Offline'}</h1>
-    </div>
+      <button onClick={statusCheck} >Toggle Status</button>
+      <br></br>
+      <br></br>
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <span className={status ? "online" : "offline"}></span>
+        <h1>{status ? "Online" : "Offline"}</h1>
+      </div>
+
     </>
   )
 }
