bca137890b2748a5e068b694dfd444ded03a862a
muskan-chouhan <mushkanc430@gmail.com>
2026-02-18 16:58:35 +0530

    create function for applying multiple styles in button dynamically

--- 28-project-status-checker/src/App.jsx
+++ 28-project-status-checker/src/App.jsx
@@ -2,34 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [status, setStatus] = useState(false);
- function checkStatus (){
-  setStatus(!status)
- }
+  const [status, setStatus] = useState("Offline");
 
-  // function goOnline() {
-  //   setStatus("Online");
-  // }
-  // function goOffline() {
-  //   setStatus("Offline");
-  // }
-  // const getButtonStyle = (type) => {
-  //   const isActive = status === type
+  function goOnline() {
+    setStatus("Online");
+  }
 
-  //   return {
-  //     backgroundColor: isActive
-  //       ? (type === "Online" ? "green" : "red")
-  //       : "#eee",
-  //     color: isActive ? "white" : "black",
-  //     border: "2px solid " + (type === "Online" ? "green" : "red")
-  //   }
-  // }
+  function goOffline() {
+    setStatus("Offline");
+  }
+  const getButtonStyle = (type) => {
+    const isActive = status === type
+
+    return {
+      backgroundColor: isActive
+        ? (type === "Online" ? "green" : "red")
+        : "#eee",
+      color: isActive ? "white" : "black",
+      border: "2px solid " + (type === "Online" ? "green" : "red")
+    }
+  }
 
   return (
     <div className="container">
-      {/* <button onClick={goOnline} style={getButtonStyle("Online")}>Go Online</button>
-      <button onClick={goOffline} style={getButtonStyle("Offline")}>Go Offline</button> */}
-     <button onClick={checkStatus}>Toggle Status</button>
+      <button onClick={goOnline} style={getButtonStyle("Online")}>Go Online</button>
+      <button onClick={goOffline} style={getButtonStyle("Offline")}>Go Offline</button>
+
       <div className="status">
         <div className={status === "Online" ? "online" : "offline"}></div>
         <h2>{status}</h2>
