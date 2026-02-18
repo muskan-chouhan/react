762714575345a558f0dc432c60aee65be8a79f16
muskan-chouhan <mushkanc430@gmail.com>
2026-02-18 17:35:25 +0530

    defined state

--- 28-project-toggle-status/src/App.jsx
+++ 28-project-toggle-status/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 
 function App() {
+  const [status,setStatus] = useState(true)
   return (
     <>
     <button>Toggle Status</button>
+    <br></br>
+    <br></br>
     </>
   )
 }
