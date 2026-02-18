02994c571f5f2147300b9fd0b8dd3d3b51a8c17f
muskan-chouhan <mushkanc430@gmail.com>
2026-02-18 16:47:18 +0530

    start toggle button add

--- 28-project-status-checker/src/App.jsx
+++ 28-project-status-checker/src/App.jsx
@@ -2,32 +2,34 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [status, setStatus] = useState("Offline");
+  const [status, setStatus] = useState(false);
+ function checkStatus (){
+  setStatus(!status)
+ }
 
-  function goOnline() {
-    setStatus("Online");
-  }
+  // function goOnline() {
+  //   setStatus("Online");
+  // }
+  // function goOffline() {
+  //   setStatus("Offline");
+  // }
+  // const getButtonStyle = (type) => {
+  //   const isActive = status === type
 
-  function goOffline() {
-    setStatus("Offline");
-  }
-  const getButtonStyle = (type) => {
-    const isActive = status === type
-
-    return {
-      backgroundColor: isActive
-        ? (type === "Online" ? "green" : "red")
-        : "#eee",
-      color: isActive ? "white" : "black",
-      border: "2px solid " + (type === "Online" ? "green" : "red")
-    }
-  }
+  //   return {
+  //     backgroundColor: isActive
+  //       ? (type === "Online" ? "green" : "red")
+  //       : "#eee",
+  //     color: isActive ? "white" : "black",
+  //     border: "2px solid " + (type === "Online" ? "green" : "red")
+  //   }
+  // }
 
   return (
     <div className="container">
-      <button onClick={goOnline} style={getButtonStyle("Online")}>Go Online</button>
-      <button onClick={goOffline} style={getButtonStyle("Offline")}>Go Offline</button>
-
+      {/* <button onClick={goOnline} style={getButtonStyle("Online")}>Go Online</button>
+      <button onClick={goOffline} style={getButtonStyle("Offline")}>Go Offline</button> */}
+     <button onClick={checkStatus}>Toggle Status</button>
       <div className="status">
         <div className={status === "Online" ? "online" : "offline"}></div>
         <h2>{status}</h2>
