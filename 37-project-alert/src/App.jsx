f673849abf610f2269e389500a3f1c6bd4e4cda1
muskan-chouhan <mushkanc430@gmail.com>
2026-02-19 14:03:48 +0530

    import alert file

--- 37-project-alert/src/App.jsx
+++ 37-project-alert/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Alert from './Alert'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <h1>Alert Project</h1>
+    <Alert/>
     </>
   )
 }
