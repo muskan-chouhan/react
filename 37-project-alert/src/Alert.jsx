b3e98c713a29430f9896019a927c4f92940d3d39
muskan-chouhan <mushkanc430@gmail.com>
2026-02-19 14:08:05 +0530

    create buttons  for show alert

--- 37-project-alert/src/Alert.jsx
+++ 37-project-alert/src/Alert.jsx
@@ -2,6 +2,10 @@ const Alert = () =>{
     return(
         <>
         <h1>Alert</h1>
+        <button>Show Success</button>
+        <button>Show Success</button>
+        <button>Show Error</button>
+        <button>Show Warning</button>
         </>
     )
 }
