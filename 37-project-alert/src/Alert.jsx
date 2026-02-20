98ada606f59c2c2d0935e9717ed97172bad0b02d
muskan-chouhan <mushkanc430@gmail.com>
2026-02-20 17:10:32 +0530

    let's start work on close button

--- 37-project-alert/src/Alert.jsx
+++ 37-project-alert/src/Alert.jsx
@@ -1,13 +1,17 @@
 import style from './css/alert.module.css'
 const Alert = ({ msg, type }) => {
 
+    const close = (msg,type) =>{
+        msg = "";
+        type = "";
+    }
     
     return (
         <>
         {/* className={styles.alert + " " + styles[type]} */}
             <div className={`${style.alert} ${style[type]}`}>
                 <span>{msg}</span>
-                <span className={style.closeBtn}>×</span>
+                <span className={style.closeBtn} onClick={()=>close(msg,type)}>×</span>
             </div>
 
 
