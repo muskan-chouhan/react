f51acc560246f30bfa2385a78bcbd94df5b3777d
muskan-chouhan <mushkanc430@gmail.com>
2026-02-20 17:07:01 +0530

    alert component not render without empty msg

--- 37-project-alert/src/Alert.jsx
+++ 37-project-alert/src/Alert.jsx
@@ -1,13 +1,13 @@
 import style from './css/alert.module.css'
 const Alert = ({ msg, type }) => {
 
+    
     return (
         <>
         {/* className={styles.alert + " " + styles[type]} */}
             <div className={`${style.alert} ${style[type]}`}>
                 <span>{msg}</span>
-                <span className={style
-                    .closeBtn}>×</span>
+                <span className={style.closeBtn}>×</span>
             </div>
 
 
