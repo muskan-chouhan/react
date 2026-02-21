82f3e2ec46344d827f19d2a2ac58b0c457f74237
muskan-chouhan <mushkanc430@gmail.com>
2026-02-21 17:16:31 +0530

    import show cards  component in app file

--- 39-project-task-board/src/App.jsx
+++ 39-project-task-board/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import TaskForm from './TaskForm'
+import ShowCard from './ShowTaskCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,8 @@ function App() {
   return (
     <>
      <h1>Task Priorty Board</h1>
-    <TaskForm/>
+     <TaskForm/>
+     <ShowCard/>
     </>
   )
 }
