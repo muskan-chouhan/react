83d67f468efa88d107b11ad9ea2ded30a659c97d
muskan-chouhan <mushkanc430@gmail.com>
2026-02-21 19:01:43 +0530

    add desgin in form

--- 39-project-task-board/src/TaskForm.jsx
+++ 39-project-task-board/src/TaskForm.jsx
@@ -4,19 +4,23 @@ const FormCard = styled.div`
       width:300px;
       padding:20px;
       border-radius:5px;
+      background:white;
+      box-shadow:0 4px 12px rgba(0,0,0,0,0.8);
+      display :flex;
+      flex-direction:column;
+      gap:15px;
+      
 `
 const TaskForm = () => {
     return (
         <FormCard>
             <form>
                 <input type='text' placeholder='Enter you Task'></input>
-                <br /><br />
                 <select>
                     <option>Low</option>
                     <option>Medium</option>
                     <option>High</option>
                 </select>
-                <br /><br />
                 <button>Add Task</button>
             </form>
         </FormCard>
