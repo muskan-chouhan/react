dc6ea899c5ed6c0ca6231373ae891750192fa540
muskan-chouhan <mushkanc430@gmail.com>
2026-02-21 17:15:21 +0530

    create show cards compnent

---
+++ 39-project-task-board/src/ShowTaskCard.jsx
@@ -0,0 +1,11 @@
+const ShowCard = () => {
+    return (
+            <div>
+                <p>Task Card</p>
+                <h1>Data</h1>
+                <h2>Low</h2><span></span>
+            </div>
+  
+    )
+}
+export default ShowCard;
