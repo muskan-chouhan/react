ba2be15c63d5e1a56cade58200b5e33db71e5afd
muskan-chouhan <mushkanc430@gmail.com>
2026-02-21 16:56:56 +0530

    create form

--- 39-project-task-board/src/App.jsx
+++ 39-project-task-board/src/App.jsx
@@ -6,6 +6,15 @@ function App() {
   return (
     <>
      <h1>Task Priorty Board</h1>
+     <form>
+      <input type='text' placeholder='Enter you Task'></input>
+      <select>
+        <option>Low</option>
+        <option>Medium</option>
+        <option>High</option>
+      </select>
+      <button>Add Task</button>
+     </form>
     </>
   )
 }
