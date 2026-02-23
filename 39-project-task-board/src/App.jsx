6169c8b4e76dfbe7c91e899d4fdfed4112cb030e
muskan-chouhan <mushkanc430@gmail.com>
2026-02-23 13:27:52 +0530

    add desgin in showcards

--- 39-project-task-board/src/App.jsx
+++ 39-project-task-board/src/App.jsx
@@ -1,32 +1,46 @@
-import { useState } from 'react'
 import TaskForm from './TaskForm'
 import ShowCard from './ShowTaskCard'
 import styled from 'styled-components'
 
-export const BoardWrapper = styled.div`
+const BoardWrapper = styled.div`
   min-height: 100vh;
   background: #f4f6f8;
   display: flex;
   justify-content: center;
 `;
 
-export const Container = styled.div`
+const Container = styled.div`
   width: 100%;
-  max-width: 1100px;
+  max-width: 1000px;
   padding: 40px 20px;
+`;
+
+const Heading = styled.h1`
+  font-size: 28px;
+  font-weight: 600;
+  margin-bottom: 30px;
+  color: #222;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
 `;
 
 function App() {
   return (
-    <>
-      <BoardWrapper>
-        <Container>
-        <h1>Task Priority Board</h1>
-        <TaskForm/>
-        <ShowCard/>
-        </Container>
-      </BoardWrapper>
-    </>
+    <BoardWrapper>
+      <Container>
+        <Heading>Task Priority Board</Heading>
+
+        <Content>
+          <TaskForm />
+          <ShowCard />
+        </Content>
+
+      </Container>
+    </BoardWrapper>
   )
 }
 
