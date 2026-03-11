c7fe866fcb6c7ec92a7a5423d1746e2c1da05c90
muskan-chouhan <mushkanc430@gmail.com>
2026-03-11 18:40:50 +0530

    add delete button and using filter we can delete data from list

--- 41-project-useref/src/App.jsx
+++ 41-project-useref/src/App.jsx
@@ -5,17 +5,23 @@ function App() {
   const inputRef = useRef(null)
   const [tasks, setTasks] = useState([])
 
-  function addTask (){ 
+  function addTask() {
     const value = inputRef.current.value
-    if(value === ""){
-   alert("Please enter task")
-     return
-}
-  setTasks(function(prev){
-  return [...prev,value]
-})
-  inputRef.current.focus()
-  inputRef.current.value = ""
+    if (value === "") {
+      alert("Please enter task")
+      return
+    }
+    // setTasks([...tasks,value])
+    setTasks(function (prev) {
+      return [...prev, value]
+    })
+    inputRef.current.focus()
+    inputRef.current.value = ""
+  }
+
+  function deleteTask(index){
+         const newArray = tasks.filter((item,i) => i !== index)
+         setTasks(newArray)
   }
   return (
     <>
@@ -24,19 +30,22 @@ function App() {
         type='text'
         placeholder='Enter Your Task'
         ref={inputRef}
-      /> 
-       <button onClick={addTask}>Task Button</button>
-{
-  tasks.length === 0
-  ?
-  <h2>No tasks yet</h2>
-  :
-  <ul>
-    {tasks.map((data,index)=>(
-      <li key={index}>{data}</li>
-    ))}
-  </ul>
-}
+      />
+      <button onClick={addTask}>Task Button</button>
+      {
+        tasks.length === 0
+          ?
+          <h2>No tasks yet</h2>
+          :
+          <ul>
+            {tasks.map((data, index) => (
+              <li key={index}>
+                {data}
+                <button onClick={() => deleteTask(index)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+      }
     </>
   )
 }
