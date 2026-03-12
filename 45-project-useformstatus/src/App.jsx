85225e3eda85341e0e6044cc6ddc8e6360823f8a
muskan-chouhan <mushkanc430@gmail.com>
2026-03-12 13:39:36 +0530

    create form

---
+++ 45-project-useformstatus/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>UseFormStatus</h1>
+      <form>
+        <input 
+          type='text'
+          placeholder='Enter Your Name'
+        />
+      </form>
+    </>
+  )
+}
+
+export default App
