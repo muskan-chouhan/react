4fafc5ba3359af271ad233b58bcb5e6ee7f49960
muskan-chouhan <mushkanc430@gmail.com>
2026-03-12 13:48:45 +0530

    create some  Fields for form

--- 45-project-useformstatus/src/App.jsx
+++ 45-project-useformstatus/src/App.jsx
@@ -8,11 +8,20 @@ function App() {
   return (
     <>
       <h1>UseFormStatus</h1>
-      <form>
+      <form action='#'>
         <input 
           type='text'
           placeholder='Enter Your Name'
+        /><br/><br/>
+        <input
+           type='email'
+           placeholder='Enter Your email'   
+        /><br/><br/>
+        <textarea
+           type='text'
+           placeholder='Enter Message'
         />
+        <button>Submit</button>
       </form>
     </>
   )
