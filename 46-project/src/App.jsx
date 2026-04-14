f987bfb30b8c0a31f3a657908a747622a694877e
muskan-chouhan <mushkanc430@gmail.com>
2026-04-14 15:20:47 +0530

    we use filter method for filtering product list

--- 46-project/src/App.jsx
+++ 46-project/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
 
 function App() {
-    const product = ["vivo",'oppo','realme']
+    const product = ["vivo",'oppo','realme','voivo1']
     const [search, setSearch] = useState("")
     function handleSearch(e){
         const value = e.target.value
         setSearch(value)
+        const filteredProducts = product.filter((item)=>{
+           return item.includes(value)
+        })
             
-    }
+    }   
     return (
         <>
         <h1>Product Search</h1>
@@ -15,10 +18,12 @@ function App() {
         placeholder="Search product..."
         onChange={handleSearch}
         />
-        <h1>{search}</h1>
+        {/* <h1>{search}</h1> */}
         </>  
 
     )
 }
 
-export default App 
+export default App;
+
+
