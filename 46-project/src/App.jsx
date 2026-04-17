1277b652f8653401a5f62127f570b383d271a1cf
muskan-chouhan <mushkanc430@gmail.com>
2026-04-17 16:00:21 +0530

    successfully show

--- 46-project/src/App.jsx
+++ 46-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 function App() {
-    const product = ["vivo",'oppo','realme','samsung','motorola''karbonn']
+    const product = ["vivo",'oppo','realme','samsung','motorola']
     const [filter,setFilter] = useState([])
 
     function handleSearch(e){
