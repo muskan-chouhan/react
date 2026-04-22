fa042d1bef575fd4a94cd17c96eb244b846a2cc7
muskan-chouhan <mushkanc430@gmail.com>
2026-04-22 18:38:00 +0530

    applying case insensitive property

--- 46-project/src/App.jsx
+++ 46-project/src/App.jsx
@@ -1,40 +1,42 @@
 import { useState } from "react"
 
 function App() {
-    const product = ["vivo",'oppo','realme','samsung','vivo1','oppo1']
-    const [filter,setFilter] = useState(product)
+  const product = ["vivo", 'oppo', 'realme', 'samsung', 'vivo1', 'oppo1', 'Samsung S23', 'OnePlus 11']
+  const [filter, setFilter] = useState(product)
 
-    function handleSearch(e){
-        const value = e.target.value
-        
-        const filteredProducts = product.filter((item)=>{
-           return item.includes(value)
-        })
-        setFilter(filteredProducts)
-            
-    }   
-    return (
-       
+  function handleSearch(e) {
+    const value = e.target.value.toUpperCase()
 
-        <>
-        <h1>Product Search</h1>
-        <input
+
+    const filteredProducts = product.filter((item) => {
+      return item.toUpperCase().includes(value)
+    })
+    setFilter(filteredProducts)
+
+  }
+
+  return (
+
+
+    <>
+      <h1>Product Search</h1>
+      <input
         placeholder="Search product..."
         onChange={handleSearch}
-        />
-<ul>
-  {filter.map((item, index) => (
-    <li key={index}>{item}</li>
-  ))}
-</ul>
+      />
+      <ul>
+        {filter.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
 
-        </>  
+    </>
 
-    )
+  )
 }
 
 export default App;
- 
+
 
 
 
