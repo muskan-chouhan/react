05c6d0695774dd8d112c25ab8b8d6cb4e987340f
muskan-chouhan <mushkanc430@gmail.com>
2026-05-04 18:23:52 +0530

    addd loading

--- 46-project/src/App.jsx
+++ 46-project/src/App.jsx
@@ -1,48 +1,40 @@
-import { useState } from "react"
-import { useTransition } from "react"
+import { useState, useTransition } from "react"
+
 function App() {
   const product = ["vivo", 'oppo', 'realme', 'samsung', 'vivo1', 'oppo1']
   const [filter, setFilter] = useState(product)
-  const [isPending, startTransition] = useTransition();
+  const [isPending, startTransition] = useTransition()
+
   function handleSearch(e) {
-    const value = e.target.value.toUpperCase().trim()  
+    const value = e.target.value.toUpperCase().trim()
 
-      
-  }
-   startTransition(() => {
-       const filteredProducts = product.filter((item) => {
-      return item.toUpperCase().includes(value)
+    startTransition(() => {
+      const filteredProducts = product.filter((item) => {
+        return item.toUpperCase().includes(value)
+      })
+      setFilter(filteredProducts)
     })
-    setFilter(filteredProducts)
-})
+  }
 
-  return (     
-    <>   
+  return (
+    <>
       <h1>Product Search</h1>
+
       <input
         placeholder="Search product..."
         onChange={handleSearch}
       />
+
+      {/* Loading UI */}
+      {isPending && <p>Loading...</p>}
+
       <ul>
         {filter.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
-
     </>
-
   )
 }
 
-export default App;
-
-
-
-
-
-
-
-
-
-
-
+export default App
